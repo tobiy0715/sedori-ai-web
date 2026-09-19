849afa69da38e8d38a2b99444431a9677c9ca03b
tobiy0715 <tobiy0715@gmail.com>
2026-09-19 20:45:55 +0900

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -71,6 +71,7 @@ export default function HomePage() {
                   day: '2-digit',
                   hour: '2-digit',
                   minute: '2-digit',
+                  second: '2-digit',
                 })
               : '';
 
